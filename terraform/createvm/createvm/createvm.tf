695a9c6b1ff7b8d77af2bd45013192fd6a22ee29
Venkatesh Peddada <venkey.peddada@gmail.com>
2022-03-30 16:34:45 +0530

    create VM terraform

---
+++ terraform/createvm/createvm/createvm.tf
@@ -0,0 +1,48 @@
+provider "google" {
+  project = var.projectid
+  region = var.region
+}
+
+resource "google_compute_instance" "default" {
+  count        = var.vmnos
+  name         = "${var.vmname}-${count.index + 1}"
+  machine_type = var.machinetype
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+    # metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+    // Apply the firewall rule to allow external IPs to access this instance
+    tags = ["http-server"]
+}
+
+# resource "google_compute_firewall" "http-server" {
+#   name    = "default-allow-http"
+#   network = "default"
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["80"]
+#   }
+
+#   // Allow traffic from everywhere to instances with an http-server tag
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags   = ["http-server"]
+# }
+
+# output "ip" {
+#   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+# }
---
+++ terraform/createvm/createvm/variable.tf
@@ -0,0 +1,58 @@
+variable "region" {
+  description = "GCP region name"
+  type        = string
+  default     = "us-west1"
+  validation {
+    condition     = can(regex("^us-west", var.region))
+    error_message = "The region should only be us-west."
+  }
+}
+
+variable "projectid" {
+  description = "GCP projectid"
+  type        = string
+  default     = "v2agent-9423a"
+}
+
+variable "zone" {
+  description = "GCP VM Zone"
+  type        = string
+  default     = "us-west1-c"
+  validation {
+    condition     = contains(["us-west1-a", "us-west1-b", "us-west1-c"], var.zone)
+    error_message = "The zone value must be us-west1-a, us-west1-b or us-west1-c only."
+  }
+}
+
+variable "vmname" {
+  description = "GCP VM Name"
+  type        = string
+  default     = "terraform-test"
+}
+
+variable "machinetype" {
+  description = "GCP VM CPU Type"
+  type        = string
+  default     = "n2-standard-2"
+  validation {
+    condition     = can(regex("^n2", var.machinetype))
+    error_message = "The machine type can only be n2."
+  }
+}
+
+variable "image" {
+  description = "GCP VM Image"
+  type        = string
+  default     = "debian-cloud/debian-9"
+  validation {
+    condition     = can(regex("^debian-cloud", var.image)) || can(regex("^centos-cloud", var.image))
+    error_message = "The image could only be a part of debian-cloud or centos-cloud image project."
+  }
+}
+
+variable "vmnos" {
+  description = "Number of VMs to create with provided config"
+  type        = string
+  default     = "2"
+  nullable = false 
+}
